fbe4c2d1ad002d3df68326dbd5f487d58ea3c3d5
patil5018 <57175615+patil5018@users.noreply.github.com>
2019-10-30 20:32:56 +0530

    Add files via upload

---
+++ Server1.java
@@ -0,0 +1,54 @@
+
+/*----------------------------------------------------------------
+Assignment No:3(B)
+Write a program using TCP sockets for wired network to implement 
+a. Peer to Peer Chat 
+
+Roll NO: 
+Batch:TEA-3
+----------------------------------------------------------------*/
+
+
+
+import java.net.*;
+import java.io.*;
+
+public class Server1{
+	public static void main(String args[])throws Exception{
+	ServerSocket ss=new ServerSocket(3344);
+	Socket s=ss.accept();
+	DataInputStream din=new DataInputStream(s.getInputStream()); 
+	DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+	BufferedReader br=new BufferedReader (new InputStreamReader(System.in));
+	String str="",str2="";
+	while(!str.equals("stop")) {
+	str= din.readUTF();
+	System.out.println("Client says: "+str);
+	str2=br.readLine();
+	dout.writeUTF(str2);
+	dout.flush();
+ }
+	din.close();
+	dout.close();
+	s.close();
+	ss.close();
+ }}
+ 
+ 
+ //*****************OUTPUT*******************
+/* 
+gescoe@gescoe-OptiPlex-3020:~/Documents/TEA84$ javac Client1.java
+gescoe@gescoe-OptiPlex-3020:~/Documents/TEA84$ java Client1
+hi
+Server says: hii
+how r u
+Server says: fine
+*/
+//****************************************************
+
+
+ 
+
+
+
+
